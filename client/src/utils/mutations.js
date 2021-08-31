8bdfb1594db3059dcb88c1fd1460acfb0c51e0fb
Alexander Medeiros <amedeiros@soundexchange.com>
2021-08-31 17:20:39 -0400

    Finish frontend

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,79 @@
+import gql from 'graphql-tag'
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      email
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+        description
+        image
+        authors
+        link
+      }
+    }
+  }
+}`
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+}`
+
+export const SAVE_BOOK = gql`
+mutation saveBook($book: BookInput!) {
+  saveBook(book: $book) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}`
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+  removeBook(bookId: $bookId) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}`
